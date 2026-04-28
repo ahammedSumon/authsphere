59ad075fc15c9952fc4357364c29db867b126ec5
Salauddin-Sumon <mynameisremo78@gmail.com>
2026-04-29 01:07:24 +0600

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: ['http://localhost:5173',
+   'https://authsphere.vercel.app', ],
   credentials: true,        
 }));
 
